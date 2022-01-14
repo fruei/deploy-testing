15f54ee450d963a029adeb93ef58adcaa8e973ac
Andres Emanuel Jara <fruei@MacBook-Air-de-Andres.local>
2022-01-14 16:57:20 -0300

    NEW: pusher chat & notifications testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,68 @@
-import { AfterViewChecked, Component, ElementRef, OnInit, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
+// import { AfterViewChecked, Component, ElementRef, OnInit, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.scss']
+// })
+
+// export class AppComponent implements AfterViewChecked{
+//   title = 'frueiApp';
+
+//   //  @ViewChildren('tlkio', {read: ElementRef}) message_bodyComp?:QueryList<ElementRef>
+
+//   // constructor(private renderer: Renderer2, private host: ElementRef) {
+//   // }
+
+//   ngAfterViewChecked() {
+//     // data-custom-css="http://localhost:4200/styles.css"
+//     // document.getElementById("tlkio")?.setAttribute('data-custom-css','https://localhost:4200/styles.css')
+//     console.log("DOM: ", document.getElementById("tlkio"));
+//     // var myobj = document.getElementById("tlkio"); //NO FUNCIONA
+//     // myobj?.remove();
+//   }
+
+//   // removeChild(){
+//   //   this.renderer.removeChild(this.host.nativeElement, this.message_bodyComp?.first.nativeElement);
+//   // }
+// }
+import { Component, OnInit } from '@angular/core';
+import Pusher from 'pusher-js';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css'],
 })
+export class AppComponent implements OnInit {
+  username?: string = 'username';
+  message?: string = '';
+  messages?: any[] = [];
+  alerts?: string = '';
 
-export class AppComponent implements AfterViewChecked{
-  title = 'frueiApp';
+  constructor(private http: HttpClient) {}
 
-  //  @ViewChildren('tlkio', {read: ElementRef}) message_bodyComp?:QueryList<ElementRef>
+  ngOnInit(): void {
+    Pusher.logToConsole = true;
 
-  // constructor(private renderer: Renderer2, private host: ElementRef) {
-  // }
+    const pusher = new Pusher('a8c12f99d5e1e2f8d1c9', {
+      cluster: 'us2',
+    });
 
-  ngAfterViewChecked() {
-    // data-custom-css="http://localhost:4200/styles.css"
-    // document.getElementById("tlkio")?.setAttribute('data-custom-css','https://localhost:4200/styles.css')
-    console.log("DOM: ", document.getElementById("tlkio"));
-    // var myobj = document.getElementById("tlkio"); //NO FUNCIONA
-    // myobj?.remove();
+    const channel = pusher.subscribe('my-channel');
+    channel.bind('my-event', (data : any) => {
+      // this.messages.push(data);
+      this.alerts = JSON.stringify(data);
+    });
   }
 
-  // removeChild(){
-  //   this.renderer.removeChild(this.host.nativeElement, this.message_bodyComp?.first.nativeElement);
-  // }
+  submit(): void {
+    this.http
+      .post('http://localhost:8000/api/messages', {
+        username: this.username,
+        message: 'echo sending message',
+      })
+      .subscribe(() => (this.message = ''));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppComponent} from './app.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -10,9 +29,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
