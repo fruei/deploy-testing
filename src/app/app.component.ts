5a05371fce40dc32733048c87cc8526f5df9876a
Emanuel Jara <emanueljara96@gmail.com>
2022-01-14 02:53:49 -0300

    Testing tlk.io

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'frueiApp';
+
+  palabra: string = "_hola mundo_";
+
+  listadoDePalabras: string[] = [ "hola 1" , "hola" , "hola", "hola 4", "hola mundo" ];
+
+  listadoDeNumeros: number[] = [ 1, 3, 5, 6, 7 ];
+
+  puedoMostrar: boolean = true;
 }
