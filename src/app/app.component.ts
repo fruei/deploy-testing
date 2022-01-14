6300ea0f4dbc132ee95d881d410fdc7e9db6c2f0
Emanuel Jara <emanueljara96@gmail.com>
2022-01-14 06:07:58 -0300

    nice try

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,26 +6,23 @@ import { AfterViewChecked, Component, ElementRef, OnInit, QueryList, Renderer2, 
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
+export class AppComponent implements AfterViewChecked{
   title = 'frueiApp';
 
-  // @ViewChildren('message_body', {read: ElementRef}) message_bodyComp?:QueryList<ElementRef>
-  // @ViewChild('message_body') private draggableElement?: ElementRef;
+  //  @ViewChildren('tlkio', {read: ElementRef}) message_bodyComp?:QueryList<ElementRef>
 
   // constructor(private renderer: Renderer2, private host: ElementRef) {
   // }
 
-  // ngAfterViewChecked() {
-  //   console.log("DOM",document.getElementById("message"))
-  //   // var myobj = document.getElementById("message");
-  //   // myobj?.remove();
-  // }
+  ngAfterViewChecked() {
+    // data-custom-css="http://localhost:4200/styles.css"
+    // document.getElementById("tlkio")?.setAttribute('data-custom-css','https://localhost:4200/styles.css')
+    console.log("DOM: ", document.getElementById("tlkio"));
+    // var myobj = document.getElementById("tlkio"); //NO FUNCIONA
+    // myobj?.remove();
+  }
 
   // removeChild(){
   //   this.renderer.removeChild(this.host.nativeElement, this.message_bodyComp?.first.nativeElement);
   // }
-  
-  ngOnInit() {
-    // this.renderer.removeChild(this.host.nativeElement,this.draggableElement?.nativeElement);
-  }
 }
