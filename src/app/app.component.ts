4e30ee33a2abd3569f883fd4fa1b63d02e7a371f
fruei <emanueljara96@gmail.com>
2022-01-17 13:06:13 -0300

    ultimos cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,6 +30,11 @@ import { Component, OnInit } from '@angular/core';
 import Pusher from 'pusher-js';
 import { HttpClient } from '@angular/common/http';
 
+export class response {
+  username?: string;
+  message?: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -38,30 +43,30 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   username?: string = 'username';
   message?: string = '';
-  messages?: any[] = [];
+  messages? : response[] = [];
   alerts?: string = '';
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     Pusher.logToConsole = true;
-
     const pusher = new Pusher('a8c12f99d5e1e2f8d1c9', {
       cluster: 'us2',
     });
 
-    const channel = pusher.subscribe('my-channel');
-    channel.bind('my-event', (data : any) => {
-      // this.messages.push(data);
-      this.alerts = JSON.stringify(data);
+    const channel = pusher.subscribe('chat');
+    channel.bind('message', (data : response) => {
+      this.messages?.push(data);
+      // this.alerts = JSON.stringify(data);
     });
   }
 
   submit(): void {
+    console.log('mensaje a enviar',this.message?.toString());
     this.http
-      .post('https://fruei.github.io/deploy-testing/api/messages', {
+      .post('http://localhost:8000/api/messages', {
         username: this.username,
-        message: 'echo sending message',
+        message: this.message,
       })
       .subscribe(() => (this.message = ''));
   }
